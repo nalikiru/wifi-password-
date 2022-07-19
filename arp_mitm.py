8c7a89e9aa63bd09b1815ab7abc114b24b33a82f
kiru <90724833+nalikiru@users.noreply.github.com>
2022-07-19 00:05:52 -0800

    arp_mitm.py

--- arp_mitm.py
+++ arp_mitm.py
@@ -164,17 +164,7 @@ def print_arp_res(arp_res):
     """ This function creates a menu where you can pick the device whose arp cache you want to poison. """
     # Program Header
     # Basic user interface header
-    print(r"""______            _     _  ______                 _           _ 
-    |  _  \          (_)   | | | ___ \               | |         | |
-    | | | |__ ___   ___  __| | | |_/ / ___  _ __ ___ | |__   __ _| |
-    | | | / _` \ \ / / |/ _` | | ___ \/ _ \| '_ ` _ \| '_ \ / _` | |
-    | |/ / (_| |\ V /| | (_| | | |_/ / (_) | | | | | | |_) | (_| | |
-    |___/ \__,_| \_/ |_|\__,_| \____/ \___/|_| |_| |_|_.__/ \__,_|_|""")
-    print("\n****************************************************************")
-    print("\n* Copyright of David Bombal, 2021                              *")
-    print("\n* https://www.davidbombal.com                                  *")
-    print("\n* https://www.youtube.com/davidbombal                          *")
-    print("\n****************************************************************")
+    print("kiru")
     print("ID\t\tIP\t\t\tMAC Address")
     print("_________________________________________________________")
     for id, res in enumerate(arp_res):
