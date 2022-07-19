60b38265968472411009f5db7c4719037191e6c5
kiru <90724833+nalikiru@users.noreply.github.com>
2022-07-18 23:58:10 -0800

    wifi-password-crack.py

--- windows10-wifi.py
+++ windows10-wifi.py
@@ -1,10 +1,3 @@
-#! py
-######################################
-#Copyright of David Bombal, 2021     #
-#https://www.davidbombal.com         #
-#https://www.youtube.com/davidbombal #
-######################################
-
 #    Import subprocess so we can use system commands.
 import subprocess
 
@@ -26,19 +19,13 @@ import re
 #    needs to be decoded before being used as a String in Python.
 command_output = subprocess.run(["netsh", "wlan", "show", "profiles"], capture_output = True).stdout.decode()
 
-#    We imported the re module to make use of regular expressions. 
-#    We want to find all the wifi names which are listed after 
-#    "ALL User Profile     :". Using regular expressions we can create 
-#    a group of all characters until the return escape sequence (\r) appears.
+
 profile_names = (re.findall("All User Profile     : (.*)\r", command_output))
 
-#    We create an empty list outside of the loop where dictionaries 
-#    containing all the wifi usernames and passwords will be saved.
+
 wifi_list = []
 
-#    If any profile names are not found this means that wifi connections 
-#    have also not been found. So we run this part to check the 
-#    details of the wifi and see whether we can get their passwords.
+
 if len(profile_names) != 0:
     for name in profile_names:
         #    Every wifi connection will need its own dictionary which 
@@ -57,16 +44,14 @@ if len(profile_names) != 0:
             #    These cases aren't absent and we should run the 
             #    "key=clear" command part to get the password.
             profile_info_pass = subprocess.run(["netsh", "wlan", "show", "profile", name, "key=clear"], capture_output = True).stdout.decode()
-            #    Again run the regular expression to capture the 
-            #    group after the : (which is the password).
+            
             password = re.search("Key Content            : (.*)\r", profile_info_pass)
             #    Check if we found a password using the regular expression. 
             #    Some wifi connections may not have passwords.
             if password == None:
                 wifi_profile["password"] = None
             else:
-                #    We assign the grouping (where the password is contained) that 
-                #    we are interested in to the password key in the dictionary.
+                
                 wifi_profile["password"] = password[1]
             #    We append the wifi information to the variable wifi_list.
             wifi_list.append(wifi_profile) 
@@ -74,3 +59,4 @@ if len(profile_names) != 0:
 for x in range(len(wifi_list)):
     print(wifi_list[x]) 
 
+#subscribe like and share
